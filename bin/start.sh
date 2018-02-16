1ddcc1394ad13fdd4d2816733f4c8a7fcf11a2fa
huggla <henrik.uggla@kristianstad.se>
2018-02-16 15:01:36 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -11,6 +11,32 @@ then
    if [ ! -s "$PGPASSFILE" ]
    then
       env -i $sudo "$SUDO_DIR/mkdir2root" "$(dirname "$PGPASSFILE")"
+      for user in $SHARE_USERS
+      do
+         user_lc=$(echo $user | xargs | tr '[:upper:]' '[:lower:]')
+         envvar="password_file_$user_lc"
+         eval "userpwfile=\$$envvar"
+         if [ -z $userpwfile ]
+         then
+            envvar="password_$user_lc"
+            eval "user_pw=\$$envvar"
+            if [ -n "$user_pw" ]
+            then
+               userpwfile=$SECRET_DIR/$user"_pw"
+               eval "echo \$$envvar > $userpwfile"
+               eval "unset $envvar"
+            else
+               echo "No password given for $user."
+               exit 1
+            fi
+         fi
+         env -i $sudo "$SUDO_DIR/addpguser" "$user" "$userpwfile" $DELETE_PASSWORD_FILES
+      done
+      
+      
+      
+      
+      
       for user in $DATABASE_USERS
       do
          env -i $sudo "$SUDO_DIR/add2dbuserlist" $user
